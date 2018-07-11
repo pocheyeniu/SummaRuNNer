99645ac78230c7bee9c5a800ead23cbc4c6c3dab
zhaopeng <zhaopeng@kaxxdeMacBook-Air.local>
2018-07-11 18:04:20 +0800

    fix bug

--- eval.py
+++ eval.py
@@ -47,54 +47,37 @@ batch_size = 1
 time1 = time.time()
 test_reader = dr.DataReader(word_tensors['test'], label_tensors['test'],
                          batch_size)
-pretrained_embedding = dr.get_embed(word_vocab)
-embedding_size = pretrained_embedding.shape[1]
-config = Config(
-        vocab_size = pretrained_embedding.shape[0],
-        embedding_dim = pretrained_embedding.shape[1],
-        position_size = 500,
-        #position_dim = 50,
-        position_dim = args.sen_len,
-        word_input_size = 150,
-        sent_input_size = 2 * args.hidden,
-        word_GRU_hidden_units = args.hidden,
-        sent_GRU_hidden_units = args.hidden,
-        pretrained_embedding = pretrained_embedding)
-
-
-with tf.Graph().as_default():
+graph = tf.Graph()
+with graph.as_default():
     session_conf = tf.ConfigProto(
       allow_soft_placement=FLAGS.allow_soft_placement,
       log_device_placement=FLAGS.log_device_placement)
     sess = tf.Session(config=session_conf)
     with sess.as_default():
         # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
-        Summa = SummaRuNNer(
-            word_vocab.size, embedding_size, pretrained_embedding
-            )
         '''
         with tf.variable_scope("Model"):
             #m = build_model(word_vocab)
             global_step = tf.Variable(0, dtype=tf.int32, name='global_step')
         '''
-        init = tf.global_variables_initializer()
-        sess.run(init)
         #saver = tf.train.Saver()
-        saver = tf.train.import_meta_graph('./runs/1528870032/checkpoints/model-1024.meta')
-        module_file = tf.train.latest_checkpoint("./runs/1528870032/" + 'checkpoints/')
+        saver = tf.train.import_meta_graph('./runs/1531124200/checkpoints/model-1024.meta')
+        module_file = tf.train.latest_checkpoint("./runs/1531124200/" + 'checkpoints/')
         saver.restore(sess, module_file)
         #saver.restore(sess, FLAGS.load_model)
         #print('Loaded model from', FLAGS.load_model, 'saved at global step', global_step.eval())
 
-
+        input_x =  graph.get_operation_by_name("inputs/x_input").outputs[0]
+        predict =  graph.get_operation_by_name("score_layer/prediction").outputs[0]
         f = codecs.open(args.model_dir+"/scores" , "w", "utf-8")
         jk = 0
+        loss_sum = 0
         for x, y in test_reader.iter():
             x = x[0]
             y = y[0]
             #print (x)
-            y_ = sess.run(Summa.y_, feed_dict = {Summa.x: x, Summa.y:y})
-            
+            y_ = sess.run(predict, feed_dict = {input_x: x})
+             
             flag = 0
             max_len = 0
             for i,item in enumerate(x):
@@ -108,10 +91,17 @@ with tf.Graph().as_default():
                 if temp == 0:
                     x = x[:i, :max_len]
                     y_ = y_[:i]
+                    y = y[:i]
                     break
                 if temp > max_len:
                     max_len = temp
             x = x[:, :max_len]
+            target = y
+            output = y_
+            epsilon = 1e-8
+            loss = tf.reduce_mean(tf.reduce_sum(-(target * tf.log(output + epsilon) + (1. - target) * tf.log(1. - output + epsilon)), axis=1)).eval()
+            print loss
+            loss_sum += loss
             #y_ = sess.run(Summa.y_, feed_dict = {Summa.x: x, Summa.y:y}
             for score in y_:
                 #print(type(score.float))
@@ -122,4 +112,5 @@ with tf.Graph().as_default():
                 #print("jk:", jk)
             f.write("\n")
         f.close()
-
+        loss_mean = loss_sum * 1.0/ test_reader.length 
+        print ("llll", loss_mean)
